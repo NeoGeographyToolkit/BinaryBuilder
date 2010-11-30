14ee15182ecdde0f354b34b7bd7762a9b67b719d
Mike Lundy <mike@fluffypenguin.org>
2010-11-29 20:31:24 -0800

    Replace osg patch with simpler one, and use OSG_LIBRARY_PATH

--- libexec-helper.sh
+++ libexec-helper.sh
@@ -7,10 +7,12 @@ fi
 case $(uname -s) in
     Linux)
         export LD_LIBRARY_PATH="$ISISROOT/lib:$ISISROOT/3rdParty/lib${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH"
+        export OSG_LIBRARY_PATH="${LD_LIBRARY_PATH}"
         ;;
     Darwin)
         export DYLD_FALLBACK_LIBRARY_PATH="$ISISROOT/lib:$ISISROOT/3rdParty/lib${DYLD_FALLBACK_LIBRARY_PATH:+:}$DYLD_FALLBACK_LIBRARY_PATH"
         export DYLD_FALLBACK_FRAMEWORK_PATH="$ISISROOT/lib:$ISISROOT/3rdParty/lib${DYLD_FALLBACK_FRAMEWORK_PATH:+:}$DYLD_FALLBACK_FRAMEWORK_PATH"
+        export OSG_LIBRARY_PATH="${DYLD_FALLBACK_LIBRARY_PATH}"
         ;;
     *)
         echo "Unknown OS: $(uname -s)" >&2
