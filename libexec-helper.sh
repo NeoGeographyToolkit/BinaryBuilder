23f09d06c1e522c8f4d20c78e0c0486dd0ba5a45
Mike Lundy <mike@fluffypenguin.org>
2011-01-10 13:54:03 -0800

    make the libexec helper properly bourne posix
    even though no supported isis os uses dash...

--- libexec-helper.sh
+++ libexec-helper.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 self=$$
-trap 'exit 1' SIGTERM
+trap 'exit 1' TERM
 
 TOPLEVEL="$(cd $(dirname $0)/.. && pwd)"
 LIBEXEC="${ASP_DEBUG_DIR:-${TOPLEVEL}/libexec}"
 
-source "${LIBEXEC}/constants.sh"
-source "${LIBEXEC}/libexec-funcs.sh"
+. "${LIBEXEC}/constants.sh"
+. "${LIBEXEC}/libexec-funcs.sh"
 
 check_isis
 set_lib_paths "${TOPLEVEL}/lib"
