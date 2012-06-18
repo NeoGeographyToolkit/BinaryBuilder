1a948d75b2b2c4679b445dd9c0b104c8b286c9fc
Zack Moratto <zachary.m.moratto@nasa.gov>
2012-06-18 09:50:33 -0700

    Add rpc_mapproject to the distribution

--- Packages.py
+++ Packages.py
@@ -222,7 +222,7 @@ class stereopipeline(GITPackage):
         self.helper('./autogen')
 
         disable_apps = 'aligndem bundleadjust demprofile geodiff isisadjustcameraerr isisadjustcnetclip plateorthoproject reconstruct results rmax2cahvor rmaxadjust stereogui'
-        enable_apps  = 'bundlevis disparitydebug hsvmerge isisadjust orbitviz orthoproject point2dem point2mesh stereo mer2camera'
+        enable_apps  = 'bundlevis disparitydebug hsvmerge isisadjust orbitviz orthoproject point2dem point2mesh stereo mer2camera rpcmapproject'
         disable_modules  = 'photometrytk controlnettk mpi'
         enable_modules   = 'core spiceio isisio sessions'
 
