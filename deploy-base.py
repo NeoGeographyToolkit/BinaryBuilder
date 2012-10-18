8bd23d746dd9aa3175a40cb8e79cec78b242189f
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2012-10-18 16:13:28 -0700

    deploy-base: Enable app dem_adjust

--- deploy-base.py
+++ deploy-base.py
@@ -150,7 +150,7 @@ if __name__ == '__main__':
                         isisadjustcnetclip plateorthoproject reconstruct results \
                         rmax2cahvor rmaxadjust stereogui'.split()
         enable_apps  = 'bundlevis disparitydebug hsvmerge isisadjust orbitviz \
-                        orthoproject point2dem point2las point2mesh stereo mer2camera'.split()
+                        orthoproject point2dem point2las dem_adjust point2mesh stereo mer2camera'.split()
         install_pkgs   = 'boost openscenegraph flapack arbitrary_qt curl  \
                           ufconfig amd colamd cholmod flann spice qwt gsl \
                           geos xercesc protobuf superlu tiff              \
