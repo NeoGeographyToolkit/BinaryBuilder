68a9ce6110ff8458d15083ef3010e9f41b3f5a0c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-04-24 12:03:45 -0700

    BB: Minor fixes

--- deploy-base.py
+++ deploy-base.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.6
+#!/usr/bin/env python
 
 from __future__ import print_function
 
@@ -18,7 +18,18 @@ from glob import glob
 global logger
 logger = logging.getLogger()
 
+def usage(msg, code):
+    parser.print_help()
+    print('\n%s' % msg)
+    sys.exit(code)
+
 if __name__ == '__main__':
+
+    code = -1
+    if sys.version_info < (2, 6):
+        print('\nERROR: Must use Python 2.6 or greater.')
+        sys.exit(code)
+
     parser = OptionParser(usage='%s tarball installdir' % sys.argv[0])
     parser.add_option('--debug',       dest='loglevel',  default=logging.INFO, action='store_const', const=logging.DEBUG, help='Turn on debug messages')
     parser.add_option("--skip-extracting-tarball",
@@ -28,21 +39,16 @@ if __name__ == '__main__':
     global opt
     (opt, args) = parser.parse_args()
 
-    def usage(msg, code=-1):
-        parser.print_help()
-        print('\n%s' % msg)
-        sys.exit(code)
-
     if not len(args) == 2:
-        usage('Missing required argument: installdir')
+        usage('Missing required argument: installdir', code)
     tarball = P.realpath(args[0])
     installdir = P.realpath(args[1])
     if not P.exists(tarball):
-        usage('Invalid tarball %s (does not exist)' % tarball)
+        usage('Invalid tarball %s (does not exist)' % tarball, code)
     if not (P.exists(installdir)):
         os.makedirs(installdir)
     if not (P.isdir(installdir)):
-        usage('Invalid installdir %s (not a directory)' % installdir)
+        usage('Invalid installdir %s (not a directory)' % installdir, code)
     logging.basicConfig(level=opt.loglevel)
 
     arch = get_platform()
