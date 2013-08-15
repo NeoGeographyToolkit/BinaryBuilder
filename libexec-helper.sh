cb264107643b99ae370aa2b1a7150eb129b1d382
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-08-15 16:27:18 -0700

    libexec: for sparse_disp must not use ASP libraries

--- libexec-helper.sh
+++ libexec-helper.sh
@@ -15,9 +15,30 @@ if [ "$(uname -s)" = "Linux" ]
 then
     check_libc
 fi
-set_lib_paths "${TOPLEVEL}/lib"
 
 PROGRAM="${LIBEXEC}/$(basename $0)"
+
+if [ "$(echo $PROGRAM | grep sparse_disp)" != "" ] &&
+    [ "$ASP_PYTHON_MODULES_PATH" != "" ]; then
+    # For sparse_disp we must not use ASP's libraries,
+    # as those don't play well with Python
+    export PYTHONPATH="$ASP_PYTHON_MODULES_PATH"
+    case $(uname -s) in
+        Linux)
+        export LD_LIBRARY_PATH="$ASP_PYTHON_MODULES_PATH"
+        ;;
+        Darwin)
+        export DYLD_FALLBACK_LIBRARY_PATH="$ASP_PYTHON_MODULES_PATH"
+        export DYLD_FALLBACK_FRAMEWORK_PATH="$ASP_PYTHON_MODULES_PATH"
+        ;;
+        *)
+        die "Unknown OS: $(uname -s)"
+        ;;
+    esac
+else
+    set_lib_paths "${TOPLEVEL}/lib"
+fi
+
 if ! test -f "${PROGRAM}"; then
     msg "Could not find ${PROGRAM}"
     die "Is your Stereo Pipeline install incomplete?"
