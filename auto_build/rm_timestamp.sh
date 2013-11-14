f71ff58a0bd8723ff17271419b10aa0580063f68
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-11-14 09:07:51 -0800

    auto_build: Added rm_timestamp.sh, harden rename_build.sh

---
+++ auto_build/rm_timestamp.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Unzip a tarball, rename it to remove the time stamp,
+# and zip it back. Thus transform
+# StereoPipeline-2.3.0-2013-11-13-x86_64-Linux-GLIBC-2.5.tar.bz2
+# into
+# StereoPipeline-2.3.0-x86_64-Linux-GLIBC-2.5.tar.bz2
+
+if [ "$#" -lt 1 ]; then echo Usage: $0 tarball; exit; fi
+
+tarball=$1
+
+if [[ ! $tarball =~ \.tar\.bz2$ ]]; then
+    echo Expecting $tarball to be a .tar.bz2 archive
+    exit 1
+fi
+
+currDir=$(pwd)
+
+tarballDir=$(dirname $tarball)
+tarballNameIn=$(basename $tarball)
+cd $tarballDir
+
+tarballNameOut=$(echo $tarballNameIn | perl -pi -e "s#\d\d\d\d-\d+-\d+-##g")
+
+# Names without the .tar.bz2 extension
+in=${tarballNameIn/.tar.bz2/}
+out=${tarballNameOut/.tar.bz2/}
+rm -rf $in $out
+
+bzip2 -dc $tarballNameIn | tar xfv - > /dev/null 2>&1
+
+mv $in $out
+
+tar cf $tarballNameOut --use-compress-prog=pbzip2 $out
+rm -rf $in $out
+
+echo Renamed $tarballDir/$tarballNameIn to $tarballDir/$tarballNameOut
+
