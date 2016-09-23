0263e8f4a742bd66dfcf08ac529cb578134b07b0
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2016-09-22 17:40:37 -0700

    rm debug statement

--- BinaryDist.py
+++ BinaryDist.py
@@ -145,7 +145,6 @@ class DistManager(object):
         for lib in self.deplist:
             for searchdir in search:
                 checklib = P.join(searchdir, lib)
-                print("search " + checklib)
                 if P.exists(checklib):
                     found.add(lib)
                     logger.debug('\tFound: %s' % checklib)
